81fb05bc9266c47b54f9f6c5b8543b832e5071c4
Maurice <maurice.nzai@gmail.com>
2024-03-04 09:24:35 +0000

    Added Task 2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighborhoodsList () {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  //const self = this; not needed coz this in arrow = this enlosing func
+  this.addNeighborhood = newNeighborhood => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
