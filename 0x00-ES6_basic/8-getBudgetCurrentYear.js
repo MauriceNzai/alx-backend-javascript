f837eb6656c43b4ef1412cc58b901bd4ad2b8058
Maurice <maurice.nzai@gmail.com>
2024-03-04 11:47:11 +0000

    Added Task 8

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+function getCurrentYear () {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear (income, gdp, capita) {
+  const suffix = getCurrentYear();
+  return {
+    [`income-${suffix}`]: income,
+    [`gdp-${suffix}`]: gdp,
+    [`capita-${suffix}`]: capita
+  };
+}
