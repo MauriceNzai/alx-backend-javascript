613577a70fb563e6e4d94f440dcdb8c16fbc68f2
Maurice <maurice.nzai@gmail.com>
2024-03-05 12:25:27 +0000

    Add Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const p1 = signUpUser(firstName, lastName);
+  const p2 = uploadPhoto(fileName);
+
+  return Promise.allSettled([p1, p2])
+    .then((values) => {
+      const result = [];
+      values.forEach((value) => {
+        result.push({
+          status: value.status,
+          value: value || value.reason.message,
+        });
+      });
+      return result;
+    });
+}
