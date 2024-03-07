e1bd51f09f46b5d534e5698f20c44687864c2e8d
Maurice <maurice.nzai@gmail.com>
2024-03-07 13:33:11 +0000

    Add Task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,20 @@
+eport default class HolbertonClass {
+  constructor (size, location) {
+    if (typeof size !== 'number') {
+      throw TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw TypeError('location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  toString () {
+    return this._location;
+  }
+
+  valueOf () {
+    return this._size;
+  }
+}
