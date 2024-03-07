db7cf7303222d6fdf462fd7fe272cd98efcbb5b1
Maurice <maurice.nzai@gmail.com>
2024-03-07 13:13:38 +0000

    Add Task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // eslint-dissable-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize; // eslint-dissable-line no-underscore-dangle
+  }
+}
