1d3fb35bdde97f7da7f4748f17aa080e8bb18eaa
Maurice <maurice.nzai@gmail.com>
2024-04-30 14:25:50 +0000

    Add Task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+async function countStudents (path) {
+  const fs = require('fs');
+  let data;
+  try {
+    data = await fs.promises.readFile(path, 'utf8');
+    const students = data.split('\n')
+      .map((student) => student.split(','))
+      .filter((student) => student.length === 4 && student[0] !== 'firstname')
+      .map((student) => ({
+        firstName: student[0],
+        lastName: student[1],
+        age: student[2],
+        field: student[3]
+      }));
+
+    const csStudents = students
+      .filter((student) => student.field === 'CS')
+      .map((student) => student.firstName);
+
+    const sweStudents = students
+      .filter((student) => student.field === 'SWE')
+      .map((student) => student.firstName);
+
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+    return { students, csStudents, sweStudents };
+  } catch {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
