bbe9f683f7fb7bc44c28a7f29877e1c6d7fdea3d
Maurice <maurice.nzai@gmail.com>
2024-05-01 10:19:51 +0000

    Update Task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 const express = require('express');
 const students = require('./3-read_file_async');
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.send('Hello Holberton School!');
 });
 app.get('/students', async (req, res) => {
-  res.writeHead(200, { 'Conten-Type': 'text/plain' });
   res.write('This is the list of our students\n');
   await students(process.argv[2]).then((data) => {
     res.write(`Number of students: ${data.students.length}\n`);
