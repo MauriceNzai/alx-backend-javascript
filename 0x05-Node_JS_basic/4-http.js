88b8a6cbcab0cd41b74de7b8ec89292636c513c9
Maurice <maurice.nzai@gmail.com>
2024-05-01 10:04:35 +0000

    Update Task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const http = require('http');
-const app = http.createServer(function (req, res) {
+
+const app = http.createServer((req, res) => {
   if (req.url) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
@@ -8,7 +9,7 @@ const app = http.createServer(function (req, res) {
   }
 });
 
-app.listen(1245, function () {
+app.listen(1245, () => {
   console.log('Server running at 127.0.0.0:1245');
 });
 
