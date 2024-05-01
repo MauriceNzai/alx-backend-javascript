59d183b396efffef316116315085d05cc369d756
Maurice <maurice.nzai@gmail.com>
2024-05-01 10:15:38 +0000

    Update Task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,8 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+app.get('/', (req, res) => { 
   res.send('Hello Holberton School!');
 });
 
