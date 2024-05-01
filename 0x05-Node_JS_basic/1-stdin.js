88b63bd05f69245a9ca883eebd29a74355f4d311
Maurice <maurice.nzai@gmail.com>
2024-05-01 09:40:23 +0000

    Update Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 process.stdout.write('Welome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function () {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
   } else {
-   // process.stdout.write('Please enter your name');
+    // process.stdout.write('Please enter your name');
   }
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
